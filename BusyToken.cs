4b71052d343c7dd168f711c31ffeee012aa1868c
Joe Cooper <joe@artofbytes.com>
2015-02-24 12:39:50 +0100

    First commit

---
+++ BusyToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BusyTokenSource
+{
+	internal class BusyToken: IDisposable
+	{
+		private readonly Action<BusyToken> onDispose;
+		public readonly string description;
+
+		public BusyToken (Action<BusyToken> onDispose, string description = null)
+		{
+			this.onDispose = onDispose;
+			this.description = description;
+		}
+
+		#region IDisposable implementation
+		public void Dispose ()
+		{
+			onDispose (this);
+		}
+		#endregion
+	}
+}
+
---
+++ BusyTokenSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BusyTokenSource
+{
+	public class BusyTokenSource
+	{
+		private readonly LinkedList<BusyToken> liveTokens = new LinkedList<BusyToken>();
+
+		public bool IsBusy {
+			get {
+				lock (liveTokens) {
+					return liveTokens.Count > 0;
+				}
+			}
+		}
+
+		public string Description {
+			get {
+				string description = null;
+				if (IsBusy) {
+					lock (liveTokens) {
+						description = liveTokens.Last.Value.description;
+					}
+				}
+				return description;
+			}
+		}
+
+		private void HandleTokenDisposal(BusyToken token)
+		{
+			lock (liveTokens) {
+				liveTokens.Remove (token);
+			}
+		}
+
+		public IDisposable GetToken(string description = null)
+		{
+			var token = new BusyToken (HandleTokenDisposal, description);
+			lock (liveTokens) {
+				liveTokens.AddLast (token);
+			}
+			return token;
+		}
+	}
+}
+
